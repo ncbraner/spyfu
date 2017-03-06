8e259eb911ada021b0600b24d859c8378efd7621
Brandon Braner <brandon@brandons-mbp.home>
2017-03-06 10:38:41 -0600

    updated link to be dynamic

--- app/templates/home.php
+++ app/templates/home.php
@@ -11,27 +11,33 @@
 
 <body>
     <style>
-    .flex{
-        display:flex;
-    }
-    .flex-center{
-        justify-content:center;
-        align-items:center;
-    }
-    form input{
-        height:20px;
-    }
-
+        .flex {
+            display: flex;
+        }
+        
+        .flex-center {
+            justify-content: center;
+            align-items: center;
+        }
+        
+        form input {
+            height: 20px;
+        }
     </style>
     <div class="container flex flex-center">
-        <form action="/" method="POST">
-            <form>
-                <div class="form-group">
-                    <h2>Please enter your sites url</h2>
-                    <label for="site">Site Url</label>
-                    <input type="text" name="url" class="form-control" id="site-url" placeholder="example.com"><button type="submit" class="btn btn-default">Submit</button>
-                </div>
-        </form>
+        <?php
+            $actionLink = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        ?>
+            <form action=<?php echo $actionLink ?> method="POST">
+                <form>
+                    <div class="form-group">
+                        <h2>Please enter your sites url</h2>
+                        <label for="site">Site Url</label>
+                        <input type="text" name="url" class="form-control" id="site-url" placeholder="example.com">
+                        <button
+                            type="submit" class="btn btn-default">Submit</button>
+                    </div>
+                </form>
 
 
     </div>
